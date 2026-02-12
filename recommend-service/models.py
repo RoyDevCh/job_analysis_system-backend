2fc96524f7b57977511dfe30d02276d661c35d1e
pehmdn8131 <126678693+pehmdn8131@users.noreply.github.com>
2026-02-12 08:35:00 +0800

    Delete recommend-service directory

--- recommend-service/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Job(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    job_name=db.Column(db.String(100))
-    skills=db.Column(db.Text)
-    salary_max=db.Column(db.Integer)
-    city=db.Column(db.String(50))
